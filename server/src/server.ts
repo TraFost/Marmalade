9e83daae603ec78e0a94ceddd6cd80d8a09e9045
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2025-11-28 22:23:29 +0700

    feat: enhance server functionality with new dependencies and middleware
    - Added new dependencies: @hono/zod-validator, drizzle-orm, pg, postgres, zod, zod-validation-error, and drizzle-kit.
    - Updated server.ts to include middleware for CORS, logging, timing, secure headers, CSRF protection, and pretty JSON responses.
    - Implemented a centralized error handling middleware to manage errors consistently across the application.
    - Refactored the main application structure to support a base API path and improved response handling.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,35 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import type { ApiResponse } from "shared/dist";
+import { csrf } from "hono/csrf";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
+import { secureHeaders } from "hono/secure-headers";
+import { timing } from "hono/timing";
+
+import errorHandler from "../middlewares/error.middleware";
+
+// import type { ApiResponse } from "shared/dist";
 
 export function createApp(): Hono {
-	const app = new Hono();
+	const api = new Hono().basePath("/api");
+	// .route("/auth", authRoutes)
 
-	app.use(cors());
+	const app = new Hono()
+		.use(cors())
+		.use(logger())
+		.use(timing())
+		.use(secureHeaders())
+		.use(csrf())
+		.use(prettyJSON())
+		.onError(errorHandler)
+		.notFound((c) =>
+			c.json({ success: false, message: "Route not found" }, 404)
+		)
+		.get("/", (c) => {
+			return c.text("Hello, Welcome to marmalade API!");
+		});
 
-	app.get("/", (c) => {
-		return c.text("Hello Hono!");
-	});
-
-	app.get("/hello", async (c) => {
-		const data: ApiResponse = {
-			message: "Hello BHVR!",
-			success: true,
-		};
-
-		return c.json(data, { status: 200 });
-	});
+	app.route("/api", api);
 
 	return app;
 }
