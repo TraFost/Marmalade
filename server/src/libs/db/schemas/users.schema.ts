8b455ff2f5156af30563395c919955ffe2d41788
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2025-11-29 13:45:47 +0700

    feat: add drizzle-zod dependency and update user schema with zod integration

--- server/src/libs/db/schemas/users.schema.ts
+++ server/src/libs/db/schemas/users.schema.ts
@@ -1,15 +1,23 @@
-import { sql } from "drizzle-orm";
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import {
+	createInsertSchema,
+	createSelectSchema,
+	createUpdateSchema,
+} from "drizzle-zod";
 
 export const users = pgTable("users", {
 	id: text("id").primaryKey().notNull(),
 	email: text("email").notNull().unique(),
 	name: text("name"),
 	image: text("image"),
-	createdAt: timestamp("created_at", { mode: "date" })
-		.notNull()
-		.default(sql`now()`),
-	updatedAt: timestamp("updated_at", { mode: "date" })
-		.notNull()
-		.default(sql`now()`),
+	createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
+	updatedAt: timestamp("updated_at", { mode: "date" }).notNull().defaultNow(),
 });
+
+const userInsertSchema = createInsertSchema(users);
+const userSelectSchema = createSelectSchema(users);
+const userUpdateSchema = createUpdateSchema(users);
+
+export type CreateUser = typeof userInsertSchema;
+export type User = typeof userSelectSchema;
+export type UpdateUser = typeof userUpdateSchema;
