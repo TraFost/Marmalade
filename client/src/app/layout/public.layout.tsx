6b3d65188c079bbac3cb29917053c1a2b3725fd7
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2025-12-08 00:07:50 +0700

    feat: add PublicLayout component for consistent layout structure

--- client/src/app/layout/public.layout.tsx
+++ client/src/app/layout/public.layout.tsx
@@ -1,23 +1,14 @@
-import type { PropsWithChildren } from "react";
 import { Outlet } from "react-router";
 
 import { Footer } from "@/shared/components/molecules/footer";
 import { Navbar } from "@/shared/components/molecules/navbar";
-import { cn } from "@/shared/lib/helper/classname";
 
-interface PublicLayoutProps extends PropsWithChildren {
-	className?: string;
-	contentClassName?: string;
-}
-
-export function PublicLayout({}: PublicLayoutProps) {
+export function PublicLayout() {
 	return (
-		<div className={cn("size-full")}>
+		<>
 			<Navbar />
-			<main className={cn("flex-1 pt-28")}>
-				<Outlet />
-			</main>
+			<Outlet />
 			<Footer />
-		</div>
+		</>
 	);
 }
