3f09f628cea6bb9f2023d52cdd0584165181b9bb
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2025-12-08 00:08:12 +0700

    refactor: update button styles for consistency and improve layout responsiveness

--- client/src/shared/components/atoms/button.tsx
+++ client/src/shared/components/atoms/button.tsx
@@ -5,17 +5,16 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/shared/lib/helper/classname";
 
 const buttonVariants = cva(
-	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 aria-invalid:border-destructive",
+	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none aria-invalid:ring-destructive/20 aria-invalid:border-destructive",
 	{
 		variants: {
 			variant: {
 				default: "bg-primary text-primary-foreground hover:bg-primary/90",
-				primary:
-					"bg-primary text-primary-foreground shadow-[0_20px_45px_-20px_rgba(242,140,40,0.8)] hover:bg-primary/90",
+				primary: "bg-primary text-primary-foreground hover:bg-primary/90",
 				destructive:
 					"bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20",
 				outline:
-					"border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground",
+					"border bg-background hover:bg-accent hover:text-accent-foreground",
 				secondary:
 					"border border-border/60 bg-primary-foreground text-foreground hover:border-primary/60 hover:bg-card",
 				ghost: "hover:text-accent-foreground",
@@ -23,8 +22,8 @@ const buttonVariants = cva(
 			},
 			size: {
 				default: "h-9 px-4 py-2 has-[>svg]:px-3",
-				sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-				lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+				sm: "h-8 rounded-full gap-1.5 px-3 has-[>svg]:px-2.5",
+				lg: "h-10 rounded-full px-6 has-[>svg]:px-4",
 				icon: "size-9",
 				"icon-sm": "size-8",
 				"icon-lg": "size-10",
